24d697a5d3010d8edb391944522621ac995e9249
David ALLIX <webda2l@gmail.com>
2024-09-16 19:11:25 +0200

    Gedmo

--- rector.php
+++ rector.php
@@ -4,27 +4,25 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\Doctrine\Set\DoctrineSetList;
-use Rector\PHPUnit\Set\PHPUnitLevelSetList;
-use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Set\ValueObject\LevelSetList;
-use Rector\Symfony\Set\SymfonyLevelSetList;
-use Rector\Symfony\Set\SymfonySetList;
 use Rector\Symfony\Set\TwigSetList;
 use Rector\ValueObject\PhpVersion;
+use Rector\Symfony\Set\SymfonySetList;
+use Rector\PHPUnit\Set\PHPUnitSetList;
 
 return RectorConfig::configure()
     ->withPhpVersion(PhpVersion::PHP_83)
     ->withPaths([
         __DIR__.'/src',
     ])
-    ->withImportNames()
+    ->withImportNames(importShortClasses: false, removeUnusedImports: true)
     ->withParallel()
     ->withSymfonyContainerXml(__DIR__.'/var/cache/dev/App_KernelDevDebugContainer.xml')
     ->withAttributesSets(symfony: true, doctrine: true, phpunit: true)
     ->withSets([
         LevelSetList::UP_TO_PHP_83,
+        SymfonySetList::SYMFONY_63,
 
-        SymfonyLevelSetList::UP_TO_SYMFONY_63,
         // SymfonySetList::SYMFONY_STRICT,
         // SymfonySetList::SYMFONY_CODE_QUALITY,
         // SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
@@ -37,7 +35,7 @@ return RectorConfig::configure()
         TwigSetList::TWIG_240,
         TwigSetList::TWIG_UNDERSCORE_TO_NAMESPACE,
 
-        PHPUnitLevelSetList::UP_TO_PHPUNIT_100,
+        PHPUnitSetList::PHPUNIT_100,
         // PHPUnitSetList::PHPUNIT_CODE_QUALITY,
         // PHPUnitSetList::PHPUNIT_YIELD_DATA_PROVIDER,
     ])
